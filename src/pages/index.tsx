9fa764e47171b10bac165cedfc838112aa7cfd88
Izaque Rodrigues <izaqueJr@users.noreply.github.com>
2021-03-29 17:45:28 -0300

    Site de noticias criado com Next.JS, Stripe para pagamentos, FaunaDb e PrismaCMS

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ig.News</title>
+      </Head>
+      <h1>
+        iae</h1>
+    </>
+  )
+}
