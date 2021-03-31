dcc08a9ae354c2f722ac14ff4f3343add5ffe972
Izaque Rodrigues <izaqueJr@users.noreply.github.com>
2021-03-31 15:32:17 -0300

    feat: :zap:
    Add next auth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next'
+import { GetStaticProps } from 'next'
 
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -39,7 +39,7 @@ export default function Home({ product }: HomeProps) {
 }
 
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
 
   const price = await stripe.prices.retrieve('price_1Ib24nIPdSZSI7PJM4c6YLdu')
 
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24 //24hours
   }
 }
